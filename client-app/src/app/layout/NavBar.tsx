5fcdc9992a6b14b5c62ca1427eaf298e0aa1963b
Mohammad Omari <momari@costar.com>
2022-02-24 13:22:38 -0500

    Error handling

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -14,9 +14,11 @@ export default function NavBar() {
                         Reactivities     
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' name='Activities'></Menu.Item>
+                <Menu.Item as={NavLink} to='/errors' name='Errors'></Menu.Item>
                 <Menu.Item>
                     <Button as={NavLink} to='/createActivity'  positive content='Create Activity'></Button>    
                 </Menu.Item>
+                
             </Container>
         </Menu>
     )
---
+++ client-app/src/features/errors/NotFound.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Segment, Header, Icon, Button } from 'semantic-ui-react';
+
+export default function NotFound() {
+    return(
+        <Segment>
+            <Header icon>
+                <Icon name='search'>
+                    Oops - we've looked everywhere and could not find this.
+                </Icon>
+            </Header>
+            <Segment.Inline>
+                <Button as={Link} to='/activities' primary>
+                    Return to activities page
+                </Button>
+            </Segment.Inline>
+        </Segment>
+        )
+}
